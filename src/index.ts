605e8a7651db43ae29e51e6c7b645035909f8ab7
Zero <zeroto521@gmail.com>
2021-01-17 16:18:24 +0800

    :tada: finish 9.6

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-import express from 'express'
+import express from 'express';
 
-import calculateBMI from './bmiCalculator'
+import calculateBMI from './calculator/bmiCalculator';
 
-const app = express()
+const app = express();
 
 app.get('/hello', (_, res) => {
-  res.send('Hello Full Stack!')
-})
+  res.send('Hello Full Stack!');
+});
 
 
 app.get('/bmi', (req, res) => {
-  const weight = Number(req.query.weight)
-  const height = Number(req.query.height)
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
 
   if (!weight || !height) {
-    res.json({ 'error': 'malformatted parameters' })
+    res.json({ 'error': 'malformatted parameters' });
   }
 
   try {
-    const bmi = calculateBMI(weight, height)
-    res.json({ weight, height, bmi })
-  } catch (e) {
-    res.json({ 'error': e.message })
+    const bmi = calculateBMI(weight, height);
+    res.json({ weight, height, bmi });
+  } catch (_) {
+    res.json({ 'error': 'malformatted parameters' });
   }
+});
 
-})
-
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
